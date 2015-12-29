0cb648f53aa0205de4a5e8f805d36896b15cb3da
Jun Lin <linjunpop@gmail.com>
2015-12-29 14:35:50 +0800

    Upgrade ecto to 2.0.

--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule KaifaLi.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,7 +31,7 @@ defmodule KaifaLi.Web do
       use Phoenix.Controller
 
       alias KaifaLi.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
 
       import KaifaLi.Router.Helpers
@@ -62,7 +63,7 @@ defmodule KaifaLi.Web do
       use Phoenix.Channel
 
       alias KaifaLi.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
     end
   end
