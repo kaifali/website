bd3a48b72b681797d3bf0ab494aab8e421d073ea
Jun Lin <linjunpop@gmail.com>
2017-08-13 14:37:21 +0800

    Update deps.

--- mix.exs
+++ mix.exs
@@ -17,27 +17,24 @@ defmodule KaifaLi.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    # TODO: remove `:connection` after postgrex released a new version.
-    # https://github.com/phoenixframework/phoenix/pull/1449#issue-124720012
     [mod: {KaifaLi, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex, :basic_auth, :connection]]
+     extra_applications: [:logger, :runtime_tools]]
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.3"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_html, "~> 2.10"},
+     {:phoenix_live_reload, "~> 1.1", only: :dev},
      {:cowboy, "~> 1.0"},
      {:basic_auth, "~> 1.0.0"},
      {:bureaucrat, "~> 0.2", only: [:test]},
